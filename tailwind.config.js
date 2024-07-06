8b22d0436490dfb1ceb40b3cddc45564bc1e5e6d
unknown <jesuiz.design@gmail.com>
2024-07-06 03:03:54 -0500

    tailwind colors config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,39 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import colors from 'tailwindcss/colors';
 import forms from '@tailwindcss/forms';
+import {
+    sky,
+    stone,
+    neutral,
+    gray,
+    slate,
+    restColors
+} from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
-        
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
 
     theme: {
-        colors: { ...colors },
+        colors: {
+            restColors,
+            sky,
+            stone,
+            neutral,
+            gray,
+            slate,
+        },
         extend: {
             scale: {
-              '120': '1.2',
-              '125': '1.25',
-              '130': '1.3',
-              '140': '1.4',
+                '120': '1.2',
+                '125': '1.25',
+                '130': '1.3',
+                '140': '1.4',
             },
             fontFamily: {
                 sans: ['Poppins', ...defaultTheme.fontFamily.sans],
